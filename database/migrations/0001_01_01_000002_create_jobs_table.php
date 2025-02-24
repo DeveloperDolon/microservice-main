9c9b4e1eb9244b35691084d1f63ce6bc46d37ffa
DeveloperDolon <dolonr718@gmail.com>
2025-02-24 23:12:38 +0600

    duplicate migration removed

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->unsignedBigInteger('id')->primary();
             $table->string('title');
             $table->string('image');
