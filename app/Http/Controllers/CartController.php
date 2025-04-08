6fc1dfd0bc23f3817ccfde1f24cc9370599fe9a0
DeveloperDolon <dolonr718@gmail.com>
2025-04-08 21:03:53 +0600

    test commit

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends BaseController
 {
     public function add(Request $request)
     {
+
+        $cartItem = $request->validate([
+            'cart_id' => 'required|exists:carts,id',
+            'product_id' => 'required|exists:carts,id',
+            'quantity' => 'required|exists:carts,id',
+            'variant_id' => 'required|exists:carts,id',
+        ]);
+
+        $cartIsExist = Cart::where('customer_id', request()->user()->id)
+            ->first();
+
+        if($cartIsExist) {
+            
+        } else {
+            $cart = Cart::create([
+                'customer_id' => request()->user()->id,
+                'price' => $cartItem['price'],
+                'discount_amount' => $cartItem['discount_amount'],
+                'coupon_applied' => $cartItem['coupon_applied'],
+                'coupon_code' => $cartItem['coupon_code'],
+            ]);
+        }
+
         $request->validate([
             'quantity' => 'integer|required',
             'price' => 'required|numeric',
