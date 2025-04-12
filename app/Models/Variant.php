e6644bb7a6dd2583abd459b17452a04a64e868ed
DeveloperDolon <dolonr718@gmail.com>
2025-04-12 22:12:49 +0600

    product create job refactor

--- app/Models/Variant.php
+++ app/Models/Variant.php
@@ -21,7 +21,7 @@ class Variant extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class);
     }
 
     static function booted()
